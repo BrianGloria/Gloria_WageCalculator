c25c6e4f79c15b5e4b3e3f301437db33e46927c8
Brian <bribriangloria@gmail.com>
2022-04-30 22:52:04 +0700

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/gloria_wagecalculator/Display.java
@@ -0,0 +1,48 @@
+package mcm.edu.ph.gloria_wagecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Display extends AppCompatActivity {
+
+    TextView txtName, txtType, txtHours, txtWage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        txtName = findViewById(R.id.txtEmployeeName);
+        txtType = findViewById(R.id.txtEmployeeType);
+        txtHours = findViewById(R.id.txtHoursRendered);
+        txtWage = findViewById(R.id.txtTotalWage);
+
+
+        Intent i = getIntent();
+        String EmployeeType = i.getStringExtra("type");
+        String EmployeeName = i.getStringExtra("empName");
+        Double EmployeeHours = Double.parseDouble(i.getStringExtra("hours"));
+
+
+        txtName.setText("Employee Name: " +EmployeeName);
+        txtType.setText("Employee Type: " + String.valueOf(EmployeeType));
+        txtHours.setText("Hours Rendered: "+ String.valueOf(EmployeeHours));
+        Double totalWage = 0.0;
+
+        if(EmployeeType.equals("Full-Time")){
+            totalWage = EmployeeHours * 100;
+            txtWage.setText("Total Wage: " +String.valueOf(totalWage));
+        }
+        else if(EmployeeType.equals("Part-time")){
+            totalWage = EmployeeHours * 75;
+            txtWage.setText("Total Wage: " +String.valueOf(totalWage));
+        }
+        else{
+            totalWage = EmployeeHours * 90;
+            txtWage.setText("Total Wage: " +String.valueOf(totalWage));
+        }
+    }
+}
---
+++ app/src/main/java/mcm/edu/ph/gloria_wagecalculator/MainActivity.java
@@ -0,0 +1,59 @@
+package mcm.edu.ph.gloria_wagecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public abstract class MainActivity extends AppCompatActivity implements View.OnLongClickListener{
+
+    EditText employeeName, employeeHours;
+    RadioGroup employeeType;
+    RadioButton btnSelected;
+    Button calculate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        employeeName = findViewById(R.id.employeeName);
+        employeeHours = findViewById(R.id.employeeHours);
+
+        employeeType = findViewById(R.id.employeeType);
+
+        calculate = findViewById(R.id.btnCalculate);
+
+
+        calculate.setOnClickListener((View.OnClickListener) this);
+
+    }
+    public void Onclick(View v) {
+
+        switch(v.getId()){
+            case R.id.btnCalculate:
+                    int selectedEmployeeType = employeeType.getCheckedRadioButtonId();
+
+                    btnSelected = findViewById(selectedEmployeeType);
+
+                    String type = btnSelected.getText().toString();
+
+                    String name = employeeName.getText().toString();
+                    Double totalHours = Double.parseDouble(employeeHours.getText().toString());
+
+                    Intent intent = new Intent(this,Display.class);
+
+                    intent.putExtra("type", type);
+                    intent.putExtra("empName", name);
+                    intent.putExtra("hours",totalHours);
+                    startActivity(intent);
+
+                break;
+        }
+    }
+}
